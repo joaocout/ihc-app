90323f48540d6fb949f53062236be244b23be65a
saulomf <saulomf91@hotmail.com>
2022-04-19 00:10:38 -0300

    Feita tela de gerencimaneto

--- src/pages/Gerenciamento/index.tsx
+++ src/pages/Gerenciamento/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, TouchableOpacity, Image } from "react-native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 import { styles } from "./styles";
 
 import type { StackParamsList } from "../../shared/types";
+import { COLORS } from "../../shared/constants";
+
+const SOFA_ICON = require("../../../assets/sofa.png");
+const CAMA_ICON = require("../../../assets/cama.png");
 
 type GerenciamentoProps = NativeStackScreenProps<
   StackParamsList,
@@ -13,8 +17,39 @@ type GerenciamentoProps = NativeStackScreenProps<
 
 const Gerenciamento = ({ navigation }: GerenciamentoProps) => (
   <View style={styles.container}>
-    <Text>Gerenciamento</Text>
-    <Button title="Relatórios" onPress={() => navigation.navigate("Cômodo")} />
+    <Text style={styles.text}>
+      Aqui estão listados os cômodos que possuem nosso sistema instalado.
+    </Text>
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => navigation.navigate("Cômodo")}
+    >
+      <Image source={SOFA_ICON} />
+      <View style={{ marginLeft: 15 }}>
+        <Text style={{ color: COLORS.WHITE, fontWeight: "bold" }}>
+          SALA DE ESTAR
+        </Text>
+        <Text style={{ color: COLORS.WHITE }}>3 aparelhos ligados</Text>
+        <Text style={{ color: COLORS.WHITE }}>MÉDIA DE CONSUMO: 3.3 kWh</Text>
+      </View>
+    </TouchableOpacity>
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => navigation.navigate("Cômodo")}
+    >
+      <Image source={CAMA_ICON} />
+      <View style={{ marginLeft: 15 }}>
+        <Text style={{ color: COLORS.WHITE, fontWeight: "bold" }}>
+          QUARTO DO VÍTOR
+        </Text>
+        <Text style={{ color: COLORS.WHITE }}>Nenhum aparelho ligado</Text>
+        <Text style={{ color: COLORS.WHITE }}>MÉDIA DE CONSUMO: 0 kWh</Text>
+      </View>
+    </TouchableOpacity>
+    <Text style={styles.textBottom}>
+      Caso deseje que nosso sistema seja instalado em outros cômodos, entre em
+      contato conosco por aqui.
+    </Text>
   </View>
 );
 
--- src/pages/Gerenciamento/styles.ts
+++ src/pages/Gerenciamento/styles.ts
@@ -1,9 +1,79 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+
+import { COLORS } from "../../shared/constants";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: COLORS.WHITE,
+    alignItems: "center",
+    paddingHorizontal: 40,
+  },
+
+  text: {
+    textAlign: "center",
+    color: COLORS.GRAY_1,
+    marginVertical: 50,
+  },
+
+  textBottom: {
+    textAlign: "center",
+    color: COLORS.BLUE,
+  },
+
+  bigNumber: {
+    fontSize: 60,
+    fontWeight: "bold",
+    alignSelf: "center",
+    borderBottomWidth: 5,
+    includeFontPadding: false,
+  },
+
+  underLine: {
+    fontSize: 80,
+    fontWeight: "bold",
+    alignSelf: "center",
+    lineHeight: 10,
+  },
+
+  textExtra: { color: COLORS.BLUE, textAlign: "center" },
+
+  mediumNumber: {
+    fontSize: 30,
+    fontWeight: "bold",
+    alignSelf: "center",
+    marginBottom: 10,
+    marginHorizontal: 10,
+  },
+
+  textMiniContainer: {
+    paddingVertical: 25,
+    textAlign: "center",
+    color: COLORS.GRAY_1,
+  },
+
+  miniContainer: {
+    flexDirection: "row",
+    marginVertical: 30,
+    marginHorizontal: 40,
+  },
+
+  button: {
+    backgroundColor: COLORS.BLUE,
+    borderRadius: 15,
+    marginBottom: 30,
+    minWidth: 340,
+    minHeight: 100,
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+
+  textButton: {
+    color: COLORS.WHITE,
+    fontWeight: "bold",
+    marginTop: 20,
   },
 });
