45d540844fa4d06c37d8f6baaf6e4ed9460f67dc
joaocout <joaopedro0801@hotmail.com>
2022-04-20 00:38:22 -0300

    graficos do relatorio

--- src/pages/Relatorios/styles.ts
+++ src/pages/Relatorios/styles.ts
@@ -1,9 +1,11 @@
 import { StyleSheet } from 'react-native';
+import { COLORS } from '../../shared/constants';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: COLORS.WHITE,
   },
 });
